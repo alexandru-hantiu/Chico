60b85728d25b0985ec93c6d92fb24b709d220278
Alexandru Hantiu <al.hantiu@gmail.com>
2023-05-28 12:57:57 +0300

    - spdlog as a submodule - wrapper over spdlog - logging macros

--- Chico/src/Chico/Entrypoint.h
+++ Chico/src/Chico/Entrypoint.h
@@ -6,9 +6,15 @@
 #ifdef CHC_PLATFORM_WINDOWS
 
 extern Chico::Application* Chico::CreateApplication();
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
 
-	printf("Chico Enginge Started\n");
+	Chico::Log::Init();
+	CHC_CORE_WARN("Initialised Log!");
+	CHC_CORE_CRITICAL("Initialised Log!");
+	int a = 5;
+	CHC_CORE_INFO("Yo! Var={0}",a);
+
 
 	auto app = Chico::CreateApplication();
 	app->Run();
